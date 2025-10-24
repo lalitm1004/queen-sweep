4e0707fe5b5c3a239cb028f537fbc2c06dd9f9e8
lalitm1004 <lalitm1004@gmail.com>
2025-10-24 05:38:41 +0000

    feat: begin hashless rewrite

--- queen-sweep-core/src/lib.rs
+++ queen-sweep-core/src/lib.rs
@@ -3,5 +3,5 @@ mod dfs;
 mod game_state;
 
 pub use cell_state::CellState;
-pub use dfs::depth_first_search;
+// pub use dfs::depth_first_search;
 pub use game_state::GameState;
--- queen-sweep-core/src/main.rs
+++ queen-sweep-core/src/main.rs
@@ -1,96 +1,98 @@
-use queen_sweep_core::{CellState, GameState, depth_first_search};
+// use queen_sweep_core::{CellState, GameState, depth_first_search};
 
-fn main() {
-    let color_regions = vec![
-        vec![0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
-        vec![0, 3, 3, 1, 1, 1, 2, 2, 2, 2, 2],
-        vec![0, 3, 3, 1, 1, 2, 2, 2, 2, 2, 2],
-        vec![0, 0, 0, 0, 2, 2, 2, 4, 4, 4, 4],
-        vec![0, 5, 5, 6, 7, 7, 2, 2, 2, 4, 4],
-        vec![0, 5, 5, 6, 7, 7, 7, 7, 7, 7, 4],
-        vec![0, 5, 5, 6, 7, 7, 7, 7, 7, 7, 4],
-        vec![0, 5, 5, 6, 7, 7, 8, 9, 7, 7, 4],
-        vec![0, 5, 5, 6, 7, 7, 8, 9, 7, 7, 8],
-        vec![10, 5, 5, 6, 7, 7, 8, 9, 7, 7, 8],
-        vec![10, 10, 10, 6, 6, 8, 8, 8, 8, 8, 8],
-    ];
+// fn main() {
+//     let color_regions = vec![
+//         vec![0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+//         vec![0, 3, 3, 1, 1, 1, 2, 2, 2, 2, 2],
+//         vec![0, 3, 3, 1, 1, 2, 2, 2, 2, 2, 2],
+//         vec![0, 0, 0, 0, 2, 2, 2, 4, 4, 4, 4],
+//         vec![0, 5, 5, 6, 7, 7, 2, 2, 2, 4, 4],
+//         vec![0, 5, 5, 6, 7, 7, 7, 7, 7, 7, 4],
+//         vec![0, 5, 5, 6, 7, 7, 7, 7, 7, 7, 4],
+//         vec![0, 5, 5, 6, 7, 7, 8, 9, 7, 7, 4],
+//         vec![0, 5, 5, 6, 7, 7, 8, 9, 7, 7, 8],
+//         vec![10, 5, 5, 6, 7, 7, 8, 9, 7, 7, 8],
+//         vec![10, 10, 10, 6, 6, 8, 8, 8, 8, 8, 8],
+//     ];
 
-    // let color_regions = vec![
-    //     vec![0, 0, 1, 1, 1, 2, 2, 2],
-    //     vec![0, 3, 1, 3, 1, 4, 2, 2],
-    //     vec![0, 3, 1, 3, 1, 2, 2, 2],
-    //     vec![0, 3, 3, 3, 1, 5, 6, 2],
-    //     vec![0, 3, 3, 3, 1, 5, 6, 6],
-    //     vec![0, 3, 7, 3, 1, 5, 6, 6],
-    //     vec![7, 3, 7, 3, 1, 5, 5, 6],
-    //     vec![7, 7, 7, 7, 6, 6, 6, 6],
-    // ];
-    let state = GameState::from_color_regions(color_regions);
-    pretty_print(&state);
+//     // let color_regions = vec![
+//     //     vec![0, 0, 1, 1, 1, 2, 2, 2],
+//     //     vec![0, 3, 1, 3, 1, 4, 2, 2],
+//     //     vec![0, 3, 1, 3, 1, 2, 2, 2],
+//     //     vec![0, 3, 3, 3, 1, 5, 6, 2],
+//     //     vec![0, 3, 3, 3, 1, 5, 6, 6],
+//     //     vec![0, 3, 7, 3, 1, 5, 6, 6],
+//     //     vec![7, 3, 7, 3, 1, 5, 5, 6],
+//     //     vec![7, 7, 7, 7, 6, 6, 6, 6],
+//     // ];
+//     let state = GameState::from_color_regions(color_regions);
+//     pretty_print(&state);
 
-    let solved = depth_first_search(state);
-    if let Some(solved) = solved {
-        pretty_print(&solved);
-    } else {
-        println!("No Solution");
-    }
-}
+//     let solved = depth_first_search(state);
+//     if let Some(solved) = solved {
+//         pretty_print(&solved);
+//     } else {
+//         println!("No Solution");
+//     }
+// }
 
-// ANSI color codes
-const RESET: &str = "\x1b[0m";
-const BLACK_FG: &str = "\x1b[30m";
-const DIM: &str = "\x1b[2m";
+// // ANSI color codes
+// const RESET: &str = "\x1b[0m";
+// const BLACK_FG: &str = "\x1b[30m";
+// const DIM: &str = "\x1b[2m";
 
-// Background colors matching Python's colorama
-const BACK_COLORS: [&str; 14] = [
-    "\x1b[101m", // LIGHTRED_EX
-    "\x1b[102m", // LIGHTGREEN_EX
-    "\x1b[103m", // LIGHTYELLOW_EX
-    "\x1b[104m", // LIGHTBLUE_EX
-    "\x1b[105m", // LIGHTMAGENTA_EX
-    "\x1b[106m", // LIGHTCYAN_EX
-    "\x1b[41m",  // RED
-    "\x1b[42m",  // GREEN
-    "\x1b[43m",  // YELLOW
-    "\x1b[44m",  // BLUE
-    "\x1b[45m",  // MAGENTA
-    "\x1b[46m",  // CYAN
-    "\x1b[47m",  // WHITE
-    "\x1b[40m",  // BLACK
-];
+// // Background colors matching Python's colorama
+// const BACK_COLORS: [&str; 14] = [
+//     "\x1b[101m", // LIGHTRED_EX
+//     "\x1b[102m", // LIGHTGREEN_EX
+//     "\x1b[103m", // LIGHTYELLOW_EX
+//     "\x1b[104m", // LIGHTBLUE_EX
+//     "\x1b[105m", // LIGHTMAGENTA_EX
+//     "\x1b[106m", // LIGHTCYAN_EX
+//     "\x1b[41m",  // RED
+//     "\x1b[42m",  // GREEN
+//     "\x1b[43m",  // YELLOW
+//     "\x1b[44m",  // BLUE
+//     "\x1b[45m",  // MAGENTA
+//     "\x1b[46m",  // CYAN
+//     "\x1b[47m",  // WHITE
+//     "\x1b[40m",  // BLACK
+// ];
 
-pub fn pretty_print(game_state: &GameState) {
-    // Print column headers
-    print!("   ");
-    for i in 0..game_state.size {
-        print!("{} ", i);
-    }
-    println!();
+// pub fn pretty_print(game_state: &GameState) {
+//     // Print column headers
+//     print!("   ");
+//     for i in 0..game_state.size {
+//         print!("{} ", i);
+//     }
+//     println!();
 
-    // Print each row
-    for r in 0..game_state.size {
-        print!("{:2} ", r);
+//     // Print each row
+//     for r in 0..game_state.size {
+//         print!("{:2} ", r);
 
-        for c in 0..game_state.size {
-            let idx = r * game_state.size + c;
-            let color_index = (game_state.colors[idx] as usize) % BACK_COLORS.len();
-            let back_color = BACK_COLORS[color_index];
+//         for c in 0..game_state.size {
+//             let idx = r * game_state.size + c;
+//             let color_index = (game_state.colors[idx] as usize) % BACK_COLORS.len();
+//             let back_color = BACK_COLORS[color_index];
 
-            match game_state.states[idx] {
-                CellState::Queen => {
-                    print!("{}{}♛ {}", back_color, BLACK_FG, RESET);
-                }
-                CellState::Blocked => {
-                    print!("{}{}{}✖ {}", back_color, BLACK_FG, DIM, RESET);
-                }
-                CellState::Empty => {
-                    print!("{}  {}", back_color, RESET);
-                }
-            }
-        }
+//             match game_state.states[idx] {
+//                 CellState::Queen => {
+//                     print!("{}{}♛ {}", back_color, BLACK_FG, RESET);
+//                 }
+//                 CellState::Blocked => {
+//                     print!("{}{}{}✖ {}", back_color, BLACK_FG, DIM, RESET);
+//                 }
+//                 CellState::Empty => {
+//                     print!("{}  {}", back_color, RESET);
+//                 }
+//             }
+//         }
 
-        println!();
-    }
+//         println!();
+//     }
 
-    println!();
-}
+//     println!();
+// }
+
+fn main() {}
