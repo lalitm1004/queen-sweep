8a2bbd51867fd2df5c69e8ac757dc93f4b08c529
lalitm1004 <lalitm1004@gmail.com>
2025-10-31 16:41:01 +0000

    misc: add microsecond display

--- queen-sweep-core/src/main.rs
+++ queen-sweep-core/src/main.rs
@@ -52,7 +52,11 @@ fn print_statistics(steps: usize, duration: std::time::Duration) {
 
     println!("Statistics:");
     println!("  Steps explored: {}", steps);
-    println!("  Time taken: {:.6} ms, {:.6} s", millis, millis / 1000.00);
+    println!(
+        "  Time taken: {:.6} ms, {:.6} µs",
+        millis,
+        duration.as_micros()
+    );
 
     if millis > 0.0 {
         let steps_per_ms = steps as f64 / millis;
